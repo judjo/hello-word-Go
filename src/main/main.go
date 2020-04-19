9abe7506dcd52f519458b987bb49d167d1752e39
docmobile17 <docmantapmobile@gmail.com>
2020-04-20 01:13:22 +0700

    update code
    update code

---
+++ src/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/contact", serveContact)
+	http.ListenAndServe(":8080", nil)
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello World!"))
+}
+
+func serveContact(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello World! This is a content page"))
+}
